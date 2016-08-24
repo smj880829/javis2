9bd454756fdaf862bdbf8dd9a32861198ad8b9e3
unknown <>
2016-08-24 20:56:24 +0900

    wow

--- routes/auth.js
+++ routes/auth.js
@@ -13,32 +13,8 @@ router.get('/login', function(req, res, next) {
   res.render('./auth/login', { title: 'WELCOME' });
 });
 
-router.get('/logintoken', function(req, res, next) {
-    token_ctl.getNewToken('123',function(token){
-      res.cookie('token',token,{expires: new Date(Date.now() + 600000),httpOnly: true,maxAge:600000})
-   })
-  res.redirect('/');
-});
-
 router.post('/login', function(req, res, next) {
-  var auth = new authorization()
-    auth.method(req.body.loginmethod)
-    .email(req.body.email)
-    .pass(req.body.password)
-    .token(req.body.accesstoken)
-    .id(req.body.id)
-    .name(req.body.name)
 
-    auth.check_user(function(re){
-          if(re){
-           token_ctl.getNewToken(req.body.email,function(token){
-             res.cookie('token',token,{expires: new Date(Date.now() + 600000),httpOnly: true,maxAge:600000})
-             res.send({'token' : token,'check':'true','email':req.body.email});
-          })
-        }else{
-            res.send({'check':'false'});
-        }
-    });
 });
 
 module.exports = router;
