6f4f21c8330d43ab1b2020adfb21182bd1598d05
unknown <>
2016-08-24 20:49:49 +0900

    wow

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var authorization = require('../authorization');
-
+var token_ctl = require('../controller_token')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,11 +11,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
   res.render('./auth/login', { title: 'WELCOME' });
-});
-
-router.get('/logintoken', function(req, res, next) {
-  res.cookie('token',token,{expires: new Date(Date.now() + 600000),httpOnly: true,maxAge:600000})
-  res.redirect('/');
 });
 
 router.post('/login', function(req, res, next) {
