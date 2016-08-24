c4fef859d1ef270eceb6a5471da2cb032a42623e
unknown <>
2016-08-24 21:29:11 +0900

    wow

--- routes/auth.js
+++ routes/auth.js
@@ -14,14 +14,18 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/logintoken', function(req, res, next) {
-    token_ctl.getNewToken('123',function(token){
-      res.cookie('token',token,{expires: new Date(Date.now() + 600000),httpOnly: true,maxAge:600000})
-   })
-  res.redirect('/');
+
 });
 
 router.post('/login', function(req, res, next) {
-
+  if(req.body.email == '123' && req.body.password == '123'){
+    token_ctl.getNewToken('123',function(token){
+      res.cookie('token',token,{expires: new Date(Date.now() + 600000),httpOnly: true,maxAge:600000})
+      res.redirect('/');
+    })
+  }else{
+    res.redirect('/auth/login');
+  }
 });
 
 module.exports = router;
