d7fa1395a41f8bf1475e178813ca1d446edea4c7
unknown <>
2016-08-24 23:22:54 +0900

    wow

--- routes/stream.js
+++ routes/stream.js
@@ -13,13 +13,13 @@ router.get('/show', function(req, res, next) {
 router.get('/show2', function(req, res, next) {
   res.render('./stream/show2');
 });
-/*
+
 router.get('/down', function(req, res, next) {
     var WebTorrent = require('webtorrent')
 
   var client = new WebTorrent()
 
-  var magnetURI = 'magnet:?xt=urn:btih:281AC5D97DD79EAEA4A8262B6C50DE5850F485EA&dn=Aino%20%20Miyuu%20XVSR-133.mp4&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2f9.rarbg.com%3a2740%2fannounce&tr=udp%3a%2f%2f9.rarbg.me%3a2780%2fannounce&tr=http%3a%2f%2f163.172.32.53%3a8000%2fannounce&tr=udp%3a%2f%2f163.172.32.53%3a8000&tr=udp%3a%2f%2fopen.nyaatorrents.info%3a6544%2fannounce'
+  var magnetURI = 'magnet:?xt=urn:btih:47D84A2A993F63D533050FE34C2C84175EA47072&dn=BBI-203.avi&tr=%2audp%3a%2f%2fopen.demonii.com%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.publicbt.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.istole.it%3a80%2fannounce&tr=udp%3a%2f%2fglotorrents.com%3a6969%2fannounce&tr=udp%3a%2f%2fopen.nyaatorrents.info%3a6544%2fannounce&tr=udp%3a%2f%2fpubt.net%3a2710%2fannounce&tr=http%3a%2f%2fpubt.net%3a2710%2fannounce&tr=udp%3a%2f%2fopen.nyaatorrents.info%3a6544%2fannounce&tr=http%3a%2f%2fwww.iamtracker.com%2fannounce.php&tr=udp%3a%2f%2f9.rarbg.com%3a2710%2fannounce&tr=udp%3a%2f%2f9.rarbg.me%3a2730%2fannounce&tr=http%3a%2f%2f163.172.32.53%3a8000%2fannounce&tr=udp%3a%2f%2f163.172.32.53%3a8000'
 
   res.render('./stream/index',{title:'succes'});
   client.add(magnetURI, { path: './files' }, function (torrent) {
@@ -30,6 +30,6 @@ router.get('/down', function(req, res, next) {
     })
   })
 });
-*/
+
 
 module.exports = router;
