7ed6ae3a8e77b5e792129cab0b5423b2f623a134
unknown <>
2016-08-24 21:29:30 +0900

    wow

--- routes/auth.js
+++ routes/auth.js
@@ -13,10 +13,6 @@ router.get('/login', function(req, res, next) {
   res.render('./auth/login', { title: 'WELCOME' });
 });
 
-router.get('/logintoken', function(req, res, next) {
-
-});
-
 router.post('/login', function(req, res, next) {
   if(req.body.email == '123' && req.body.password == '123'){
     token_ctl.getNewToken('123',function(token){
